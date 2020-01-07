90f2e441ff9cc94b1014ff0a78b8058cb9f84715
Dennis Gosnell <cdep.illabout@gmail.com>
2020-01-07 22:21:46 +0900

    update

--- rust/atbash-cipher/src/lib.rs
+++ rust/atbash-cipher/src/lib.rs
@@ -1,6 +1,15 @@
+fn normalize(str: &str) -> String {
+    str.chars().filter(|c| c.is_alphanumeric()).flat_map(char::to_lowercase).collect()
+}
+
+fn encode_cipher_chars(str: String) -> String {
+    
+}
+
 /// "Encipher" with the Atbash cipher.
 pub fn encode(plain: &str) -> String {
-    unimplemented!("Encoding of {:?} in Atbash cipher.", plain);
+    // TODO: Just write this as a map?
+    encode_cipher_chars(normalize(plain))
 }
 
 /// "Decipher" with the Atbash cipher.
