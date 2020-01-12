dbe97cb5df17230f42ea1958e8e239535dbc665d
Dennis Gosnell <cdep.illabout@gmail.com>
2020-01-12 22:59:30 +0900

    update

--- rust/space-age/src/lib.rs
+++ rust/space-age/src/lib.rs
@@ -26,11 +26,43 @@ pub struct Saturn;
 pub struct Uranus;
 pub struct Neptune;
 
-impl Planet for Mercury { fn years() -> f64 { 0.2408467 } }
-impl Planet for Venus { fn years() -> f64 { 0.61519726 } }
-impl Planet for Earth { fn years() -> f64 { 1.0 } }
-impl Planet for Mars { fn years() -> f64 { 1.8808158 } }
-impl Planet for Jupiter { fn years() -> f64 { 11.862615 } }
-impl Planet for Saturn { fn years() -> f64 { 29.447498 } }
-impl Planet for Uranus { fn years() -> f64 { 84.016846 } }
-impl Planet for Neptune { fn years() -> f64 { 164.79132 } }
+impl Planet for Mercury {
+    fn years() -> f64 {
+        0.2408467
+    }
+}
+impl Planet for Venus {
+    fn years() -> f64 {
+        0.61519726
+    }
+}
+impl Planet for Earth {
+    fn years() -> f64 {
+        1.0
+    }
+}
+impl Planet for Mars {
+    fn years() -> f64 {
+        1.8808158
+    }
+}
+impl Planet for Jupiter {
+    fn years() -> f64 {
+        11.862615
+    }
+}
+impl Planet for Saturn {
+    fn years() -> f64 {
+        29.447498
+    }
+}
+impl Planet for Uranus {
+    fn years() -> f64 {
+        84.016846
+    }
+}
+impl Planet for Neptune {
+    fn years() -> f64 {
+        164.79132
+    }
+}
