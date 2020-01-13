6dcbfc3b2a31265a14b09bd4da53b5c36a8b0243
Dennis Gosnell <cdep.illabout@gmail.com>
2020-01-13 18:55:35 +0900

    update

--- rust/space-age/src/lib.rs
+++ rust/space-age/src/lib.rs
@@ -11,9 +11,9 @@ impl From<u64> for Duration {
 }
 
 pub trait Planet {
-    fn years() -> f64;
+    const YEARS: f64;
     fn years_during(d: &Duration) -> f64 {
-        d.0 / Self::years()
+        d.0 / Self::YEARS
     }
 }
 
@@ -26,43 +26,11 @@ pub struct Saturn;
 pub struct Uranus;
 pub struct Neptune;
 
-impl Planet for Mercury {
-    fn years() -> f64 {
-        0.2408467
-    }
-}
-impl Planet for Venus {
-    fn years() -> f64 {
-        0.61519726
-    }
-}
-impl Planet for Earth {
-    fn years() -> f64 {
-        1.0
-    }
-}
-impl Planet for Mars {
-    fn years() -> f64 {
-        1.8808158
-    }
-}
-impl Planet for Jupiter {
-    fn years() -> f64 {
-        11.862615
-    }
-}
-impl Planet for Saturn {
-    fn years() -> f64 {
-        29.447498
-    }
-}
-impl Planet for Uranus {
-    fn years() -> f64 {
-        84.016846
-    }
-}
-impl Planet for Neptune {
-    fn years() -> f64 {
-        164.79132
-    }
-}
+impl Planet for Mercury { const YEARS: f64 = 0.2408467; }
+impl Planet for Venus { const YEARS: f64 = 0.61519726; }
+impl Planet for Earth { const YEARS: f64 = 1.0; }
+impl Planet for Mars { const YEARS: f64 = 1.8808158; }
+impl Planet for Jupiter { const YEARS: f64 = 11.862615; }
+impl Planet for Saturn { const YEARS: f64 = 29.447498; }
+impl Planet for Uranus { const YEARS: f64 = 84.016846; }
+impl Planet for Neptune { const YEARS: f64 = 164.79132; }
