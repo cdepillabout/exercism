15a17a2d3ffa9245ee5b92359010264177d7c574
Dennis Gosnell <cdep.illabout@gmail.com>
2020-01-15 12:49:32 +0900

    update

--- rust/minesweeper/src/lib.rs
+++ rust/minesweeper/src/lib.rs
@@ -1,8 +1,8 @@
 #[macro_use]
 extern crate itertools;
 
-use std::collections::HashMap;
 use std::collections::hash_map::Entry;
+use std::collections::HashMap;
 use std::hash::Hash;
 
 #[derive(Debug, Copy, Clone)]
@@ -23,22 +23,22 @@ enum Piece {
     Empty,
 }
 
-
 fn create_updates(row_index: usize, col_index: usize) -> Vec<BoardUpdate> {
     let row_min = if row_index == 0 { 0 } else { row_index - 1 };
     let col_min = if col_index == 0 { 0 } else { col_index - 1 };
 
     iproduct!(row_min..(row_index + 2), col_min..(col_index + 2))
-        .map(|(row, col)|
+        .map(|(row, col)| {
             if row == row_index && col == col_index {
                 BoardUpdate::BoardUpdate(BoardUpdateType::Mine, row, col)
             } else {
                 BoardUpdate::BoardUpdate(BoardUpdateType::Update, row, col)
-            })
+            }
+        })
         .collect()
 }
 
-fn map_row(row_index: usize, row: &str) -> Vec<BoardUpdate> {
+fn calc_updates_for_row(row_index: usize, row: &str) -> Vec<BoardUpdate> {
     row.chars()
         .enumerate()
         .flat_map(|(col_index, item): (usize, char)| match item {
@@ -54,8 +54,9 @@ fn singleton_vec<A>(a: A) -> Vec<A> {
     vec
 }
 
-fn hashmap_from_assoc_list<A, B> (v: Vec<(A, B)>) -> HashMap<A, Vec<B>> where
-    A: Eq + Hash
+fn hashmap_from_assoc_list<A, B>(v: Vec<(A, B)>) -> HashMap<A, Vec<B>>
+where
+    A: Eq + Hash,
 {
     let mut hashmap: HashMap<A, Vec<B>> = HashMap::new();
 
@@ -73,14 +74,16 @@ fn hashmap_from_assoc_list<A, B> (v: Vec<(A, B)>) -> HashMap<A, Vec<B>> where
     hashmap
 }
 
-fn invert_board_update(board_updates: &[BoardUpdate]) -> HashMap<(usize, usize), Vec<BoardUpdateType>> {
+fn invert_board_update(
+    board_updates: &[BoardUpdate],
+) -> HashMap<(usize, usize), Vec<BoardUpdateType>> {
     hashmap_from_assoc_list(
         board_updates
             .iter()
-            .map(|BoardUpdate::BoardUpdate(board_update_type, row, col)|
+            .map(|BoardUpdate::BoardUpdate(board_update_type, row, col)| {
                 ((*row, *col), *board_update_type)
-                )
-            .collect()
+            })
+            .collect(),
     )
 }
 
@@ -94,9 +97,7 @@ fn add_update(piece: Piece, board_update_type: &BoardUpdateType) -> Piece {
 }
 
 fn board_updates_to_piece(board_updates: &[BoardUpdateType]) -> Piece {
-    board_updates
-        .iter()
-        .fold(Piece::Empty, add_update)
+    board_updates.iter().fold(Piece::Empty, add_update)
 }
 
 fn piece_to_str(piece: Option<&Piece>) -> String {
@@ -107,42 +108,47 @@ fn piece_to_str(piece: Option<&Piece>) -> String {
     }
 }
 
-fn pieces_to_board(max_row: usize, max_col: usize, pieces: &HashMap<(usize, usize), Piece>) -> Vec<String> {
+fn pieces_to_board(
+    max_row: usize,
+    max_col: usize,
+    pieces: &HashMap<(usize, usize), Piece>,
+) -> Vec<String> {
     (0..max_row)
-        .map(|r|
+        .map(|r| {
             (0..max_col)
                 .map(|c| piece_to_str(pieces.get(&(r, c))))
                 .collect::<String>()
-        )
+        })
         .collect::<Vec<String>>()
 }
 
 pub fn annotate(minefield: &[&str]) -> Vec<String> {
     let num_row: usize = minefield.len();
-    let num_col: usize =
-        match minefield.get(0) {
-            None => return Vec::new(),
-            Some(row) => row.len(),
-        };
+    let num_col: usize = match minefield.get(0) {
+        None => return Vec::new(),
+        Some(row) => row.len(),
+    };
 
-    let all_updates: Vec<BoardUpdate> =
-        minefield
-            .iter()
-            .enumerate()
-            .flat_map(|(row_index, row): (usize, &&str)| {
-                map_row(row_index, *row)
-            })
-            .collect();
+    // Calculate all the updates for each square on the board.
+    let all_updates: Vec<BoardUpdate> = minefield
+        .iter()
+        .enumerate()
+        .flat_map(|(row_index, row): (usize, &&str)| calc_updates_for_row(row_index, *row))
+        .collect();
 
+    // Invert the updates and index by the square being updated.
     let mut inverted_board_updates: HashMap<(usize, usize), Vec<BoardUpdateType>> =
         invert_board_update(&all_updates);
 
-    let pieces: HashMap<(usize, usize), Piece> =
-        inverted_board_updates
-            .iter_mut()
-            .map(|(idx, board_updates): (&(usize, usize), &mut Vec<BoardUpdateType>)|
-                (*idx, board_updates_to_piece(board_updates)))
-            .collect();
+    // Calculate the final piece for each square that will get an update.
+    let pieces: HashMap<(usize, usize), Piece> = inverted_board_updates
+        .iter_mut()
+        .map(
+            |(idx, board_updates): (&(usize, usize), &mut Vec<BoardUpdateType>)| {
+                (*idx, board_updates_to_piece(board_updates))
+            },
+        )
+        .collect();
 
     pieces_to_board(num_row, num_col, &pieces)
 }
